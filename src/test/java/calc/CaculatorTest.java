c4c16ef58be9330556499e9c8e6dd8ac1202c22b
naylanasir <48768519+naylanasir@users.noreply.github.com>
2019-03-21 21:55:56 +0100

    Update CaculatorTest.java

--- src/test/java/calc/CaculatorTest.java
+++ src/test/java/calc/CaculatorTest.java
@@ -21,20 +21,5 @@ public class CaculatorTest {
 	public void testSub() {
 		assertEquals(1, calc.sub(3,2));
 	}
-/*	@Test
-	public void testMul() {
-		assertEquals(6, calc.mul(3,2));
-	}
-	@Test
-	public void testDiv() {
-		assertEquals(1, calc.div(2,2));
-	}
-	@Test
-	public void testDivByZero() {
-		assertEquals(0, calc.div(3,0));
-	}
-	@Test
-	public void testPresedence() {
-		assertEquals(30, calc.add(calc.mul(5, 5), calc.div(10, 2)));
-	}*/
+
 }
