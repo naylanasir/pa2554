01f8039514dd818111c62204b46f2fbdd7b34c30
naylanasir <48768519+naylanasir@users.noreply.github.com>
2019-03-21 22:26:19 +0100

    Fist-Commit

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -7,14 +7,4 @@ public class CalculatorMain {
 	public int add(int a, int b){
 		return a+b;
 	}
-	public int subb(int a, int b){
-		return a-b;
-	}
-	public int mull(int a, int b){
-		return a*b;
-	}
-	public int div(int a, int b)
-	{
-		return a/b;
-	}
 }
