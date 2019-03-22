f13c5bc67886d0c969d09b8c9ab425b148a4414e
naylanasir <48768519+naylanasir@users.noreply.github.com>
2019-03-22 19:10:10 +0100

    Updated main class

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -1,18 +1,20 @@
 package calc;
 
 public class CalculatorMain {
-	public CalculatorMain(){
-		
-	}
+	public CalculatorMain(){}
+	
 	public int add(int a, int b){
 		return a+b;
 	}
+	
 	public int sub(int a, int b){
 		return a-b;
 	}
+	
 	public int mul(int a, int b){
 		return a*b;
 	}
+	
 	public int div(int a, int b){
 		if(b==0) return 0;
 		return a/b;
